8e882548a70d9ada48183b348fa20763021add50
Matin Delkhosh <bootimat@gmail.com>
2024-10-23 19:59:38 +0330

    maze img

--- Online Task/PsychoPy project/OverthinkingModel_lastrun.py
+++ Online Task/PsychoPy project/OverthinkingModel_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.1post4),
-    on October 22, 2024, at 15:12
+    on October 23, 2024, at 19:50
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
